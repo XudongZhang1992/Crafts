6bf8e5a8fa5b24c1fa109b98d8f99e25ccaf475a
Ran Cui <ran.cui@hotmail.com>
2017-12-17 11:47:49 +0800

    add some constant values

--- TrouserTech.java
+++ TrouserTech.java
@@ -1,15 +1,26 @@
 /*
  * This is the class that abstract the calculation of trousers manufacture parameters.
  * 
+ * Note that all the numerics are in Chinese "cun".
+ * 
  * Created by Xudong Zhang and Ran Cui on 2017/12/13.
 */
 
 public class TrouserTech {
+    // some fixed length in "cun"
+    public static final double WAIST_LENGTH = 1.0;
+    public static final double CHA_LENGTH = 1.5;
 
-    // these input sizes are in Chinese "chi", except for foot
+    // num of needles to add when making crotch, depending on gender
+    public static final int MALE_ADD_NEEDLE = 4;
+    public static final int FEMALE_ADD_NEEDLE = 6;
+
+    // all in Chinese "cun" for calculation simplicity
+    // vertical
     private double length; 
     private double crotch;
-    private double foot; // this is in "cun"
+    // horizontal
+    private double foot;
     private double ass;
 
     private double rhoRotation; // how many rotations == 1 "cun"?
@@ -26,10 +37,11 @@ public class TrouserTech {
     public TrouserTech(double length_, double crotch_, double foot_, double ass_,
                        double rhoR, double rhoN, int machineT, int footT, 
                        boolean addE, boolean isM, boolean isBI) {
-        this.length = length_;
-        this.crotch = crotch_;
+        // length, crotch and ass are in "chi", hence * 10
+        this.length = length_ * 10;
+        this.crotch = crotch_ * 10;
         this.foot = foot_;
-        this.ass = ass_;
+        this.ass = ass_ * 10;
         this.rhoRotation = rhoR;
         this.rhoNeedle = rhoN;
         this.machineType = machineT;
